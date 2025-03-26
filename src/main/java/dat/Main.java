32c92e337b6bb9b0839b5e48d1105e80f8762501
ash2213 <157792270+ash2213@users.noreply.github.com>
2025-03-26 23:48:34 +0100

    v22

--- src/main/java/dat/Main.java
+++ src/main/java/dat/Main.java
@@ -11,6 +11,8 @@ public class Main {
     public static void main(String[] args) {
         int port = 7000;
 
+
+
         ApplicationConfig.getInstance()
                 .initiateServer()
                 .setRoute(RouteDefinitions.getRoutes())  // Brug routes fra separat klasse
