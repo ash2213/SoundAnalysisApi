e74894d1773fb5703568b259a8094f8e05994a4b
ash2213 <157792270+ash2213@users.noreply.github.com>
2025-03-28 11:33:07 +0100

    v27

--- src/main/java/dat/Main.java
+++ src/main/java/dat/Main.java
@@ -12,7 +12,7 @@ public class Main {
     public static void main(String[] args) {
       ConnectionPool connectionPool = new ConnectionPool();
 
-        int port = 7001;
+        int port = 7000;
 
 
 
