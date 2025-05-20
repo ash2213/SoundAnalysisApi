97ddc54e454a59c555d8ad1e5588b6f7abc93ac2
ash2213 <157792270+ash2213@users.noreply.github.com>
2025-05-20 12:11:17 +0200

    v33

--- src/test/java/dat/AudioEndpointsTest.java
+++ src/test/java/dat/AudioEndpointsTest.java
@@ -1,28 +1,44 @@
 package dat.routes;
 
 import dat.config.ApplicationConfig;
+import dat.config.HibernateConfig;
 import dat.routes.RouteDefinitions;
-import io.javalin.Javalin;
 import io.restassured.RestAssured;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
 import static io.restassured.RestAssured.*;
 import static org.hamcrest.Matchers.*;
 
+@Testcontainers
 class AudioEndpointsTest {
+
+    // Starter en PostgreSQL-container automatisk
+    @Container
+    static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("postgres:16")
+            .withDatabaseName("test_db")
+            .withUsername("testuser")
+            .withPassword("testpass");
 
     private static ApplicationConfig app;
 
     @BeforeAll
     static void setup() {
-       app= ApplicationConfig.getInstance()
+        // Fortæl Hibernate vi er i testmiljø
+        HibernateConfig.setTest(true);
+
+        // Start Javalin server med test-DB
+        app = ApplicationConfig.getInstance()
                 .initiateServer()
-                .setRoute(RouteDefinitions.getRoutes()) // hvis du bruger ConnectionPool kan du mocke eller oprette test-db
+                .setRoute(RouteDefinitions.getRoutes())
                 .handleException()
                 .startServer(7002);
 
+        // REST-assured opsætning
         RestAssured.baseURI = "http://localhost";
         RestAssured.port = 7002;
         RestAssured.basePath = "/api";
@@ -43,7 +59,6 @@ class AudioEndpointsTest {
                 .body("status", equalTo("success"))
                 .body("files", notNullValue());
     }
-
 
     @Test
     void showGraph_invalidId() {
